ae9f5a195a96d03a2425a2216a28ffbfac0f5d6e
kartikeyachaauhan <kc1.408596@gmail.com>
2019-06-04 07:18:08 +0530

    add product control

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 import './products.dart';
 
+import 'product_control.dart';
+
 class ProductManager extends StatefulWidget {
+
   final String startingProduct;
 
   ProductManager({this.startingProduct = 'Sweets Tester'});
@@ -23,21 +26,26 @@ class _ProductManagerState extends State<ProductManager> {
   }
 
   @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void _addProduct(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Column(children: [Container(
-      margin: EdgeInsets.all(10.0),
-      child: RaisedButton(
-        color: Theme.of(context).primaryColor,
-        onPressed: () {
-          setState(() {
-            _products.add('Advanced Food Tester');
-          });
-        },
-        child: Text('Add Product'),
-      ),
-    ),
-    Products(_products)
-    ],);
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: ProductControl(_addProduct),
+        ),
+        Products(_products)
+      ],
+    );
   }
 }
